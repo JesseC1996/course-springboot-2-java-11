c90f127437a0258ce6ed0f71236206cf7937d14c
JesseC1996 <lavolpeblue@gmail.com>
2021-01-27 19:53:19 -0300

    Project Created

---
+++ src/main/java/com/jessec/course/CourseApplication.java
@@ -0,0 +1,13 @@
+package com.jessec.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
